5bee7c7ed021606ac4bde3866ac34497373792bf
Mixty <138919084+KittyGirlthing@users.noreply.github.com>
2026-02-14 12:55:42 -0800

    Use redis for session management

--- keys.py
+++ keys.py
@@ -5,5 +5,5 @@ URL_HOST: str = getenv('URL_HOST', "http://localhost:5000")
 OAUTH2_CLIENT_ID: str = getenv('OAUTH2_CLIENT_ID')
 OAUTH2_CLIENT_SECRET: str = getenv('OAUTH2_CLIENT_SECRET')
 OAUTH2_REDIRECT_URI: str = getenv('OAUTH2_REDIRECT_URI', f"{URL_HOST}/callback")
-
 DISCORD_API_BASE_URL = getenv('API_BASE_URL', 'https://discordapp.com/api')
+WEB_APP_SECRET_KEY = getenv('WEB_APP_SECRET_KEY')

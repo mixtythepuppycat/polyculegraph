77bb4e815e3471a14ad3bf8c1a3085f79ae6a0a1
Mixty <138919084+KittyGirlthing@users.noreply.github.com>
2026-02-13 16:03:39 -0800

    Container fixes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cloudflared tunnel run --token $TOKEN &
-gunicorn --conf gunicorn_conf.py --bind 0.0.0.0:5000 app:app &
+#cloudflared tunnel run --token $TOKEN &
+gunicorn --conf gunicorn_conf.py --bind 0.0.0.0:5000 web:app &
 python bot.py
